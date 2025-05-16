6a99bc70d9a21faf2917f007cdac68ba2778270c
Luciana Sanchez <uwu13099@gmail.com>
2025-05-16 10:27:30 -0500

    docs(update): update login para q acceda al api

--- js/Login.js
+++ js/Login.js
@@ -5,3 +5,30 @@ document.getElementById("togglePassword").addEventListener("click", function () 
   passwordInput.setAttribute("type", type);
   this.textContent = type === "password" ? "👁" : "🙈";
 });
+
+document.getElementById("login-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const usernameInput = document.getElementById("username").value;
+    const passwordInput = document.getElementById("password").value;
+
+    fetch("https://68270fef397e48c913186ce3.mockapi.io/api/usuarios")
+      .then(response => response.json())
+      .then(usuarios => {
+        // Verificar si el usuario existe con ese nombre y contraseña
+        const usuarioEncontrado = usuarios.find(usuario =>
+          usuario.email === usernameInput && usuario.password === passwordInput
+        );
+
+        if (usuarioEncontrado) {
+          alert("Inicio de sesión exitoso");
+          localStorage.setItem("nombreUsuario", usuarioEncontrado.nombre);
+          window.location.href = "inicio.html";
+        } else {
+          alert("Nombre o contraseña incorrectos");
+        }
+      })
+      .catch(error => {
+        console.error("Error al conectarse con la API:", error);
+        alert("Hubo un problema al verificar tus datos.");
+      });
+  });
